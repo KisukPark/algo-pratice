64fc35e0bfb21a1dcb36776c06a9ffd142dcde12
Kisuk <kisuk0521@nethru.co.kr>
2015-06-24 01:46:50 +0900

    FixParen complete

--- Fixparen/fixparen.rb
+++ Fixparen/fixparen.rb
@@ -1,3 +1,5 @@
+$left = {"(" => ")", "{" => "}", "[" => "]", "<" => ">"}
+$right = {")" => "(", "}" => "{", "]" => "[", ">" => "<"}
 $priority = []
 
 def initPriorityArray(priorityStr)
@@ -13,6 +15,21 @@ def getPriority(paren)
   return $priority.index(paren)
 end
 
+def parens2str(parens)
+    r = nil
+    o = parens.first
+    c = parens.last
+
+    if getPriority(o) <= getPriority($right[c])
+        r = [$right[c]] + parens[1..parens.length-2] + [c]
+    else
+        r = [o] + parens[1..parens.length-2] + [$left[o]]
+    end
+
+    return r.join
+end
+
+
 gets.to_i.times {
   args = gets.strip.split(" ")
   mismatched = args[0]
@@ -20,5 +37,26 @@ gets.to_i.times {
 
   parens = []
 
+  mismatched.length.times do |i|
+      paren = mismatched[i]
 
+      if ($left.has_key? paren)
+          #left
+          parens.push(paren)
+      else
+          #right
+          parens.reverse.each_with_index do |pa, i|
+              if pa.length == 1 && $left.has_key?(pa)
+
+                  ei = parens.length - 1
+                  si = ei - i
+                  r = parens2str(parens[si..ei].push(paren))
+                  parens[si..ei] = r
+                  break
+              end
+          end
+      end
+  end
+
+  puts parens.join
 }
